7c2fa8effeea1f0ec0c6f86905905b1901ecfadb
Brady <bjgebbie@gmail.com>
2023-07-09 14:31:31 -0500

    DCF Calculator added

--- fin-tool-app/src/components/TenK.js
+++ fin-tool-app/src/components/TenK.js
@@ -1,32 +1,114 @@
 import React, { Component } from 'react';
-
+import '../models/DCF';
+import DCF from '../models/DCF';
+//65824
+//12734
+//67155
 export default class TenK extends Component{
-    constructor() {
-        super();
-        this.state = { data: [] };
-      }
-    async getThing() {
-        const url = 'https://twelve-data1.p.rapidapi.com/symbol_search?symbol=AA&outputsize=30';
-        const options = {
-            method: 'GET',
-            headers: {
-            'X-RapidAPI-Key': '67d13b4d03msh4c308b39cdedb1dp145c47jsnf25c62aaffac',
-            'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
-            }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            ocf: '',
+            dr: '',
+            mce: '',
+            p2fcf: '',
+            growthRate: '',
+            n: '',
+            csms: ''
         };
-        
+    
+        this.handleOcfChange = this.handleOcfChange.bind(this);
+        this.handleDrChange = this.handleDrChange.bind(this);
+        this.handleMceChange = this.handleMceChange.bind(this);
+        this.handleP2fcfChange = this.handleP2fcfChange.bind(this);
+        this.handleGrowthRateChange = this.handleGrowthRateChange.bind(this);
+        this.handleNChange = this.handleNChange.bind(this);
+        this.handleCsmsChange = this.handleCsmsChange.bind(this);
+      }
+    
+      handleOcfChange(event) {
+        this.setState(
+            {
+                ocf: event.target.value,
+            }
+        );
+      }
 
-        const response = await fetch(url, options);
-        console.log(response);
-        const result = response.json();
-        console.log(result);
-        return JSON.stringify(result);        
-    }
-    render() {
+      handleMceChange(event) {
+        this.setState(
+            {
+                mce: event.target.value,
+            }
+        );
+      }
+
+      handleDrChange(event) {
+        this.setState(
+            {
+                dr: event.target.value,
+            }
+        );
+      }
+    
+      handleP2fcfChange(event) {
+        this.setState(
+            {
+                p2fcf: event.target.value,
+            }
+        );
+      }
+
+      handleGrowthRateChange(event) {
+        this.setState(
+            {
+                growthRate: event.target.value,
+            }
+        );
+      }
+
+      handleNChange(event) {
+        this.setState(
+            {
+                n: event.target.value,
+            }
+        );
+      }
+
+      handleCsmsChange(event) {
+        this.setState(
+            {
+                csms: event.target.value,
+            }
+        );
+      }
+
+      render() {
         return (
             <div>
-                { this.getThing() }
+                <form>
+                    <div className="form-group" style={{textAlign: "center"}}>
+                        <p>Operating Cash Flow: <input type="text" value={this.state.ocf} onChange={this.handleOcfChange} /></p>
+                        <p>Discount Rate: <input type="text" value={this.state.dr} onChange={this.handleDrChange} /></p>
+                        <p>Maintenance Capital Expenditure: <input type="text" value={this.state.mce} onChange={this.handleMceChange} /></p>
+                        <p>Price to FCF: <input type="text" value={this.state.p2fcf} onChange={this.handleP2fcfChange} /></p>
+                        <p>Growth Rate: <input type="text" value={this.state.growthRate} onChange={this.handleGrowthRateChange} /></p>
+                        <p>How many years: <input type="text" value={this.state.n} onChange={this.handleNChange} /></p>
+                        <p>Cash and short-term: <input type="text" value={this.state.csms} onChange={this.handleCsmsChange} /></p>                
+                    </div>
+                    <div>
+                        {new DCF(
+                                Number(this.state.ocf), 
+                                Number(this.state.mce),
+                                Number(this.state.dr),
+                                Number(this.state.p2fcf),
+                                Number(this.state.growthRate),
+                                Number(this.state.csms),
+                                Number(this.state.n)).calculate().toString()}
+                    </div>
+                </form>
             </div>
-        )
-    }
+          
+        );
+      }
 }
--- fin-tool-app/src/models/DCF.js
+++ fin-tool-app/src/models/DCF.js
@@ -1,6 +1,6 @@
 export default class DCF {
     /**
-     * ocf: operating cash flow
+     * ocf: operating cash flow 
      * dr: discount rate (return you want to achieve)
      * p2fcf: price to free cash flow (rn cvs is 5.24 need to make function to calculate this number later)
      * growth: growth rate of the company
@@ -16,24 +16,34 @@ export default class DCF {
      *  5. Add csms to dcf
      *  6. dcf / total shares outstanding yields fair price
      */
-    constructor(ocf, dr, p2fcf, growth, mce, csms, n) {
+    constructor(ocf, mce, dr, p2fcf, growthRate, csms, n) {
         this.ocf = ocf;
+        this.mce = mce;
         this.dr = dr;
         this.p2fcf = p2fcf;
-        this.growth = growth;
-        this.mce = mce;
+        this.growthRate = growthRate;
         this.csms = csms;
         this.n = n;
     }
 
     calculate() {
         let fcf = this.ocf - this.mce;
-        let fcfList = []
-        for (let i = 0; i < this.n; i++) {
-            fcf = fcf * growth;
-            fcfList.push(fcf);
+        let sumOfDcf = 0;
+        let dcf = 0;
+        let termVal = 0;
+        let discTermVal = 0; 
+        for (let i = 1; i <= this.n; i++) {
+            
+            fcf = fcf * (this.growthRate + 1);
+            dcf = (fcf / Math.pow(1 + this.dr, i)); 
+            sumOfDcf = dcf + sumOfDcf;
+
         }
-        const tv = fcf * this.p2fcf;
+
+        termVal = fcf * this.p2fcf;
+        discTermVal = termVal / Math.pow(1 + this.dr, this.n)
+
+        return sumOfDcf + discTermVal + this.csms;
     }
     
 }
